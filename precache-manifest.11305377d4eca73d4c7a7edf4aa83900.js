d1a5f0c415859590c7759ea4d93941057491079d
Juan Mosquera <maxiplux@gmail.com>
2020-05-31 00:20:13 -0400

    Updates

---
+++ precache-manifest.11305377d4eca73d4c7a7edf4aa83900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff5d7d20677c7e3b276fc7eb0fcf0ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb24daa4affad295cc72",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "78c449330cb1fbd31421",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "cb24daa4affad295cc72",
+    "url": "/static/js/2.9c637e62.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9c637e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c449330cb1fbd31421",
+    "url": "/static/js/main.3947235d.chunk.js"
+  },
+  {
+    "revision": "67091fe6125469712005",
+    "url": "/static/js/runtime-main.73e6bbb6.js"
+  }
+]);
